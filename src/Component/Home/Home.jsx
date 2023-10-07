66851c23a46e624ae2dc551ce0064ca71db06596
arif1313 <arifhossen13131@gmail.com>
2023-10-08 00:24:49 +0600

    search control complete

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,24 +1,60 @@
+
 import { useLoaderData } from "react-router-dom";
-import Banar from "../Banar/Banar";
-
-
 import Card from "../Card/Card";
-
-
+// import BannarSearch from "../BannarSearch/BannarSearch";
+import { useState } from "react";
 
 const Home = () => {
-    const cards = useLoaderData();
+    const loadalldata = useLoaderData();
+    const cards = loadalldata.donations;
  
-   
+    const [searchCata, setCata]=useState();
+    const [seaccard, setSearchcard]= useState(cards);
+    const hanleSubmit=e=>{
+        e.preventDefault();
+      setCata(e.target.text.value);
+      const manageCards= cards.filter(acard=>acard.category === searchCata)
+    setSearchcard(manageCards);
+
+    }
+  
   
     return (
         <div>
-            
-            <Banar></Banar>
+            {/* banar */}
+            <div>
+    
+    <div className="hero min-h-screen" style={{backgroundImage: 'url(./images/headerBg.png)'}}>
+      
+  <div className="hero-overlay bg-white bg-opacity-90"></div>
+  <div className="hero-content text-center text-neutral-content">
+  
+    <div className="justify-center">
+   
+      <h1 className="mb-5 text-5xl text-black font-bold ">I Grow By Helping People In Need</h1>
+     <div className="sm:ml-12 md:ml-36">
+        {/* banarseach */}
+        <div className="flex ">
+        <form action="" onSubmit={hanleSubmit} >
+           
+           <input  type="text" name="text" placeholder="Search here" className="input input-bordered w-full max-w-xs mr-7" /> 
+           <input type="submit" className="btn bg-[#FF444A] "/>
          
+           </form>
+           </div>
+     {/* <BannarSearch></BannarSearch> */}
+     </div>
+    </div>
+  </div>
+</div>
+
+</div>
+            {/* <Banar></Banar> */}
+         {/* banarclose */}
            <div className="grid sm:grid-cols-1 lg:grid-cols-4 md:grid-cols-2   gap-7 max-auto container mb-7">
            {
-              cards.donations.map((card,inx)=><Card key={inx} card={card}></Card>)
+            //  cards.donations.find((card,inx)=>card.category===searchCata? <Card key={inx} card={card}></Card>:'')
+            seaccard.map((card,inx)=><Card key={inx} card={card}></Card>)
             }
            </div>
           
