8d81cddca488e48e83b956e5a9039977be23e7b5
arif1313 <arifhossen13131@gmail.com>
2023-10-07 19:29:03 +0600

    chart legend and percent added

--- src/Component/Statistics/Statistics.jsx
+++ src/Component/Statistics/Statistics.jsx
@@ -1,43 +1,66 @@
 import { useEffect, useState } from "react";
 import getStoreddata from "../../utilities/getelemen";
-import { PieChart, Pie, Cell,  } from 'recharts';
+import { PieChart, Pie, Cell,Legend } from 'recharts';
 
 const Statistics = () => {
-
+    
     const [mydonatNumber , setDOntenumber]=useState();
     useEffect(()=>{
         const myDonations = getStoreddata();
         setDOntenumber(myDonations.length)
-        console.log(mydonatNumber);
+        console.log('may d',mydonatNumber);
 
     },[])
-
     const data = [
-        { name: 'alldonation', value: 12 },
-        { name: 'Mydonation', value: 5 }
+        { name: 'aTotal Donation', value: 12 },
+        { name: 'Your Donation', value: 5 }
        
       ];
-      const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+     
+      const COLORS = ['#00C49F',  '#FF8042'];
+
+      const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  console.log('value of x ',x);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent*100).toFixed(0)}%`}
+    </text>
+  );
+};
     return (
-        <div>
-          
-        
+        <div className="felx justify-between">
+       
+       
+        <div className="ml-96">
+         
         <PieChart width={400} height={400}>
+        <Legend /> 
           <Pie
             data={data}
             cx="50%"
             cy="50%"
+            label={renderCustomizedLabel}
             fill="#8884d8"
             // label={renderCustomizedLabel}
            
-          
+            labelLine={false}
             dataKey="value"
           >
+               
+            <Legend />  
+            
           {data.map((entry, index) => (
               <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
             ))}
           </Pie>
-        </PieChart>
+        </PieChart> 
+       
+        </div>
      
         </div>
     );
