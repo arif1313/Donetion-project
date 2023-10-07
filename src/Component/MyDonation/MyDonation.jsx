5830f0b1c3633c370a9e755ca52f8c2bd0155ed7
arif1313 <arifhossen13131@gmail.com>
2023-10-07 12:59:23 +0600

    my donation section complete to data display

--- src/Component/MyDonation/MyDonation.jsx
+++ src/Component/MyDonation/MyDonation.jsx
@@ -1,11 +1,28 @@
 
 
-const MyDonation = () => {
+import PropTypes from 'prop-types';
+const MyDonation = ({singleCard}) => {
+     const {id,title, image, category,button_background_colour,background_colour, text_colour,price}= singleCard;
     return (
         <div>
-            
+          <div className='flex rounded-lg 'style={{background: background_colour}}>
+            <div className='h-56 w-48 rounded-lg'>
+                    <img className='w-full h-full rounded-l-lg'  src={image} alt="" />
+            </div>
+            <div className='p-5'>
+            <div style={{ backgroundColor: button_background_colour ,color:text_colour }} className=' border-none py-1 px-2 rounded font-medium w-fit' >{category}</div>
+                    <h1 className='text-2xl font-semibold my-2'> {title}</h1>
+                    <h3 style={{color:text_colour} } className='mb-2 font-semibold text-lg'>$ {price}</h3>
+                    <button className='normal-case btn text-white font-semibold my-2 text-lg rounded-md'style={{background: text_colour}}> View Details</button>
+
+            </div>
+          </div>
         </div>
     );
+};
+
+MyDonation.propTypes = {
+ singleCard:PropTypes.object   
 };
 
 export default MyDonation;
--- src/Component/MyDonations/MyDonations.jsx
+++ src/Component/MyDonations/MyDonations.jsx
@@ -1,20 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import getStoreddata from "../../utilities/getelemen";
+import MyDonation from "../MyDonation/MyDonation";
 
 
 const MyDonations = () => {
+    const [matchCards, setMatchCards]=useState([]);
    const allCards= useLoaderData()
    console.log('all card display in my donations',allCards)
    useEffect(()=>{
     const mylocalCard =getStoreddata();
     console.log('my localdat',mylocalCard);
+    console.log('my localdat length',mylocalCard.length);
+    console.log('my all length',allCards.donations
+    .length);
+
+    
+
+    if(allCards.donations
+        .length > 0){
+        const matchAlldata = allCards.donations.filter(matchData=>mylocalCard.includes(matchData.id))
+        setMatchCards(matchAlldata)
+     
+    }
+   
    },[])
+   console.log('matched data',matchCards)
     return (
-        <div>
-            <h1>this is details section</h1>
+        <div className="grid grid-cols-2 gap-7 mx-auto container ">
+           {
+           matchCards.map((singleCard,ind)=><MyDonation key={ind} singleCard={singleCard}></MyDonation>)
+           }
         </div>
     );
 };
-
+{/*  */}
 export default MyDonations;
